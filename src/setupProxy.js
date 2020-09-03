896b649fcda1e46a56a3d6a5716db2083ef73d51
MichealYahn <15202205796@163.com>
2020-09-03 09:05:30 +0800

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,8 +5,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = function (app) {
     // proxy第一个参数为要代理的路由
     // 第二参数中target为代理后的请求网址，changeOrigin是否改变请求头，其他参数请看官网
-    app.use(createProxyMiddleware('/gov_open', {
-        target: 'http://zw.enorth.com.cn',
+    app.use(createProxyMiddleware('/sha', {
+        target: 'http://shashasha.com',
         changeOrigin: true
     }))
 }
